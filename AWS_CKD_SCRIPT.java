81fe7034cb46d5d5cc57850a5a6fbfc160589431
pavanchowdary501 <95085061+pavanchowdary501@users.noreply.github.com>
2023-03-28 12:58:03 +0800

    Update AWS_CKD_SCRIPT.java

--- AWS_CKD_SCRIPT.java
+++ AWS_CKD_SCRIPT.java
@@ -1,1 +1,95 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as batch from 'aws-cdk-lib/aws-batch';
+import * as github from 'aws-cdk-lib/aws-codecommit';
+
+const app = new cdk.App();
+
+const stack = new cdk.Stack(app, 'MyStack', {
+  env: {
+    account: '1234567890',
+    region: 'us-west-2',
+  },
+});
+
+// Create a VPC with a single public subnet
+const vpc = new ec2.Vpc(stack, 'MyVpc', {
+  maxAzs: 1,
+});
+
+// Create a batch job queue and compute environment
+const jobQueue = new batch.JobQueue(stack, 'MyJobQueue', {
+  computeEnvironments: [
+    {
+      type: batch.ComputeEnvironmentType.MANAGED,
+      computeEnvironmentName: 'MyComputeEnvironment',
+      managed: {
+        instanceType: new ec2.InstanceType('m5.large'),
+        minvCpus: 2,
+        maxvCpus: 10,
+        desiredvCpus: 4,
+        instanceRole: new iam.Role(stack, 'MyComputeEnvironmentRole', {
+          assumedBy: new iam.ServicePrincipal('batch.amazonaws.com'),
+        }),
+      },
+    },
+  ],
+});
+
+// Create an ECR repository to store Docker images
+const ecrRepo = new ecr.Repository(stack, 'MyEcrRepo');
+
+// Create a CodeBuild project to build the Docker image and push it to ECR
+const codeBuildProject = new codebuild.Project(stack, 'MyCodeBuildProject', {
+  source: codebuild.Source.gitHub({
+    owner: 'my-github-org',
+    repo: 'my-github-repo',
+    webhookFilters: [
+      codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH).andBranchIs('main'),
+    ],
+    webhook: true,
+  }),
+  environment: {
+    buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+    privileged: true,
+    environmentVariables: {
+      IMAGE_REPO_NAME: {
+        value: ecrRepo.repositoryUri,
+      },
+    },
+  },
+  buildSpec: codebuild.BuildSpec.fromObject({
+    version: '0.2',
+    phases: {
+      pre_build: {
+        commands: [
+          'echo Logging in to Amazon ECR...',
+          'aws --version',
+          '$(aws ecr get-login --region $AWS_REGION --no-include-email)',
+        ],
+      },
+      build: {
+        commands: [
+          'echo Build started on `date`',
+          'echo Building the Docker image...',
+          'docker build -t $IMAGE_REPO_NAME:latest .',
+          'docker tag $IMAGE_REPO_NAME:latest $IMAGE_REPO_NAME:$IMAGE_TAG',
+        ],
+      },
+      post_build: {
+        commands: [
+          'echo Build completed on `date`',
+          'echo Pushing the Docker images to the Amazon ECR repository...',
+          'docker push $IMAGE_REPO_NAME:latest',
+          'docker push $IMAGE_REPO_NAME:$IMAGE_TAG',
+        ],
+      },
+    },
+    artifacts:
 
